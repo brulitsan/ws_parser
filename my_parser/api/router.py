8a10e7380a06105b2defdcad1d136905a0e79a3c
matve <matveytulaevm.ama@gmail.com>
2024-01-11 17:34:43 +0300

    create parser#2

--- my_parser/api/router.py
+++ my_parser/api/router.py
@@ -1,8 +1,7 @@
 import fastapi
 
-from my_parser.api.handlers import get_coins, trigger_parser
+from my_parser.api.handlers import get_coins
 
 router = fastapi.APIRouter()
 
 router.add_api_route(path='/coins', methods=["GET"], endpoint=get_coins)
-router.add_api_route(path="/trigger_parses", methods=["GET"], endpoint=trigger_parser)
