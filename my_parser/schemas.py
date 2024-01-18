ba602cd08c0b134e29926b63a50a20ffd06a3156
matve <matveytulaevm.ama@gmail.com>
2024-01-18 13:49:06 +0300

    fix

--- my_parser/schemas.py
+++ my_parser/schemas.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
+from pydantic.v1 import BaseSettings
 
 
 class CoinData(BaseModel):
     name: str
     price: float
+
+
+class Settings(BaseSettings):
+    mongo_url: str
+    db_name: str
+    collection_name: str
+
+    class Config:
+        env_file = ".env"
